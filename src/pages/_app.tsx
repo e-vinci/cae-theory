903b33bca75c443e2cbc8dc1ffa6c8253b77c7c8
e-baron <raphael.baroni@vinci.be>
2025-01-29 10:38:39 +0100

    style: add global table css with mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "./code-block.css";
 import "prism-themes/themes/prism-vsc-dark-plus.css"; // Import Prism CSS
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { GlobalStyles } from "@mui/material";
 
 const theme = createTheme({
   palette: {
@@ -24,6 +25,31 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <GlobalStyles
+        styles={{
+          table: {
+            width: '100%',
+            borderCollapse: 'collapse',
+          },
+          th: {
+            border: `1px solid ${theme.palette.divider}`,
+            padding: '8px',
+            backgroundColor: theme.palette.primary.main,
+            color: theme.palette.primary.contrastText,
+            fontWeight: 'bold',
+          },
+          td: {
+            border: `1px solid ${theme.palette.divider}`,
+            padding: '8px',
+          },
+          'tr:nth-of-type(even)': {
+            backgroundColor: theme.palette.action.hover,
+          },
+          'tr:hover': {
+            backgroundColor: theme.palette.action.selected,
+          },
+        }}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
